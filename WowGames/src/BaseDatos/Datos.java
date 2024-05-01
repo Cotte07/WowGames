19fbb3a03b341ce7ac8aae1f97e38a170a8007b7
Nicolas <nprada1@udi.edu.co>
2024-05-01 02:15:58 -0500

    Avance 8
    Conexion bd - crecion Ventanas

--- WowGames/src/BaseDatos/Datos.java
+++ WowGames/src/BaseDatos/Datos.java
@@ -1,7 +1,23 @@
 package BaseDatos;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Datos {
 
-	String datos;
-	String conexion; 
+	private String conectionstr = "jdbc:oracle:thin:@192.168.254.215:1521:orcl";
+	private String username = "";
+	private String password = "";
+	
+	public Connection getConnection() {
+		Connection conn = null;
+		try {
+			conn = DriverManager.getConnection(this.conectionstr, this.username, this.password);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return conn;
+	}
 }
